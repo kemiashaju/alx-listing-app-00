0bccd60a7c88a6dffaea79168ebf4d2ae370ae6c
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-18 10:58:52 +0100

    Layout

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-6 mt-10">
+      <div className="max-w-7xl mx-auto text-center">
+        <p>© 2025 StayFinder. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
